af9cafb853d8e64e3364b66b9bda861d45e623aa
tobiasvavroch <tbv@email.cz>
2023-02-25 23:28:00 +0100

    .

--- test.py
+++ test.py
@@ -1,25 +1,3 @@
-# from datetime import datetime, timedelta
-
-# def split_date_interval(start_date, end_date):
-#     # Convert input strings to datetime objects
-#     start_datetime = datetime.strptime(start_date, '%Y-%m-%d')
-#     end_datetime = datetime.strptime(end_date, '%Y-%m-%d')
-#     end_datetime = end_datetime+ timedelta(days=1)
-
-
-#     # Calculate the number of 7-day intervals between the two dates
-#     num_weeks = int((end_datetime - start_datetime).days / 7)
-
-#     # Create a list of dates by adding 7 days to the start date for each interval
-#     date_list = []
-#     for i in range(num_weeks + 1):
-#         date_list.append((start_datetime + timedelta(weeks=i)).strftime('%Y-%m-%d'))
-#     date_list.append(end_datetime.strftime('%Y-%m-%d'))
-
-#     return date_list
-
-
-# print(split_date_interval('2022-02-02', '2022-02-20'))
 
 from server import app
 import unittest
